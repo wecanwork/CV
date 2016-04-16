12752dd014173008f74a679639bf205d18caa996
lsphillips <lsphillips.mail@gmail.com>
2016-04-16 15:20:33 +0100

    The first version of my CV.

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function (grunt)
+{
+	// Grunt Dependencies
+	// -------------------------------------------------------
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-appcache');
+	grunt.loadNpmTasks('grunt-open');
+
+	// Configuration
+	// -------------------------------------------------------
+
+	grunt.initConfig(
+	{
+		less :
+		{
+			options :
+			{
+				report : 'gzip'
+			},
+
+			build :
+			{
+				files :
+				{
+					'build/css/cv.css' : 'less/cv.less'
+				},
+
+				options : { compress : true }
+			}
+		},
+
+		// ------------------------------------------------------
+
+		appcache :
+		{
+			options :
+			{
+				basePath : 'build'
+			},
+
+			build :
+			{
+				dest : 'build/manifest.appcache',
+
+				cache :
+				{
+					patterns : ['build/**'],
+				}
+			}
+		},
+
+		// ------------------------------------------------------
+
+		copy :
+		{
+			build :
+			{
+				files :
+				[
+					{ expand : true, src : ['assets/**', 'index.html'], dest : 'build' }
+				]
+			}
+		},
+
+		// ------------------------------------------------------
+
+		open :
+		{
+			build :
+			{
+				path : 'build/index.html'
+			}
+		},
+
+		// ------------------------------------------------------
+
+		watch :
+		{
+			build : { files : ['assets/**', 'less/**', 'index.html'], tasks : ['less:build', 'copy:build', 'appcache:build'] }
+		}
+	});
+
+	// Task: `build`
+	// -------------------------------------------------------
+
+	grunt.registerTask('build', ['less:build', 'copy:build', 'appcache:build', 'open:build']);
+
+	// Task: `default`
+	// -------------------------------------------------------
+
+	grunt.registerTask('default', ['build']);
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# My CV
+
+The source code for my CV.
+
+## Development
+
+Grunt is used to handle the build process for my CV. To perform a full build, use the build task:
+
+```
+grunt build
+```
+
+This will run also open the CV in your default browser where you can view, print or save it.
